3c91788ac708f432de529118e6939b2639d52685
dainbow <suslov9876@gmail.com>
2021-11-10 19:42:13 +0300

    Added head, free, tail, chainNum indicators at graph

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -11,19 +11,48 @@ char* MakeListGraph(List* list, const char name[]) {
     FILE* output = fopen(endName, "w");
     assert(output != nullptr);
 
-    fprintf(output, "digraph G{ \n");
+    fprintf(output, "digraph G{ \n"
+                    "newrank=true;\n");
+
     fprintf(output, "\t0[shape=record,label= \"<d0>%d | <n0>%d | <p0>%d\"];\n", 
             list->chains[0].data, list->chains[0].next, list->chains[0].previous);
+    fprintf(output, "subgraph cluster0 {\n"
+		"style = filled;\n"
+		"color = lightgray;\n"
+		"0;\n"
+		"label = \"NULL_CHAIN\";"
+	"}\n");
 
     for (uint32_t curChain = 1; curChain < list->memoryAmount; curChain++) {
-        fprintf(output, "\t%u[shape=record,label= \"<d%u>%d | <n%u>%d | <p%u>%d\"];\n", 
+        fprintf(output, "\t%u[shape=record, style = rounded, label= \"<d%u>%d | <n%u>%d | <p%u>%d\"];\n", 
                 curChain, 
                 curChain, list->chains[curChain].data, 
                 curChain, list->chains[curChain].next, 
                 curChain, list->chains[curChain].previous);
 
-        fprintf(output, "\t%u->%u[color=\"White\"];\n", 
+        fprintf(output, "\t%u->%u[style=invis];\n", 
                 curChain - 1, curChain);
+
+        fprintf(output, "subgraph cluster%u {\n"
+		"style = filled;\n"
+		"color = lightgray;\n"
+		"%u;\n"
+		"label = \"%u\";"
+	"}\n", curChain, curChain, curChain);
+
+        if (curChain == list->head) {
+                fprintf(output, "head[label = \"HEAD\", color = red, style = rounded];\n");
+                fprintf(output, "{rank = same; %u; head}\n", curChain);
+        }
+        if (curChain == list->free) {
+                fprintf(output, "free[label = \"FREE\", color = red, style = rounded];\n");
+                fprintf(output, "{rank = same; %u; free}\n", curChain);
+        }
+        if (curChain == list->tail) {
+                fprintf(output, "tail[label = \"TAIL\", color = red, style = rounded];\n");
+                fprintf(output, "{rank = same; %u; tail}\n", curChain);
+        }
+
         if (list->chains[curChain].previous != -1) {
             fprintf(output, "\t%u:<n%u> ->%d: <p%d>[color=\"Green\"];\n", 
                     curChain, curChain, 
